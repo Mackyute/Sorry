ab8edabc8811d75f960a6b3a8fe24fa633951ffa
Mark Lester Baldonaza <baldonaza.marklester@dfcamclp.edu.ph>
2025-02-01 13:06:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('mainButton').addEventListener('click', function() {
+    document.getElementById('loveLetter').classList.remove('hidden');
+});
+
+document.getElementById('flowerButton').addEventListener('click', function() {
+    window.location.href = 'pink flower2.html'; // Replace with your desired link
+});
